fc79529ddc7a9e11d415cf1a862b4aaeea22342c
Christopher Sporn <djacidck@googlemail.com>
2026-07-28 16:00:29 +0000

    Tierdaten in HTML einfügen dynamisch

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,21 +10,44 @@ def load_data(file_path):
 animals_data = load_data("animals_data.json")
 
 
+animals_info = ""
+
 for animal in animals_data:
-    print()
+    animals_info += '<li class="cards__item">\n'
 
-    # Name
     if "name" in animal:
-        print(f"Name: {animal['name']}")
+        animals_info += f'<div class="card__title">{animal["name"]}</div>\n'
 
-    # Diet
+    animals_info += '<div class="card__text">\n'
+
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        print(f"Diet: {animal['characteristics']['diet']}")
+        animals_info += f'Diet: {animal["characteristics"]["diet"]}<br>\n'
 
-    # Location (erster Eintrag)
     if "locations" in animal and animal["locations"]:
-        print(f"Location: {animal['locations'][0]}")
+        animals_info += f'Location: {animal["locations"][0]}<br>\n'
 
-    # Type
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        print(f"Type: {animal['characteristics']['type']}")
+        animals_info += f'Type: {animal["characteristics"]["type"]}<br>\n'
+
+    animals_info += "</div>\n"
+    animals_info += "</li>\n"
+
+
+# Bestehendes Template lesen
+with open("animals_template.html", "r") as file:
+    html_content = file.read()
+
+
+# Platzhalter ersetzen
+html_content = html_content.replace(
+    "__REPLACE_ANIMALS_INFO__",
+    animals_info
+)
+
+
+# Bestehende Datei überschreiben
+with open("animals_template.html", "w") as file:
+    file.write(html_content)
+
+
+print("animals_template.html wurde aktualisiert!")
