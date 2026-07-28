9d4d814326b1f947fde8e06e1fe3c2d1fb51528b
Christopher Sporn <djacidck@googlemail.com>
2026-07-28 16:06:00 +0000

    verbessere HTML der Karten-Elemente

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,7 +11,7 @@ def load_data(file_path):
 animals_data = load_data("animals_data.json")
 
 
-# HTML-Code für die Tiere erzeugen
+# HTML-String für die Tiere erzeugen
 animals_info = ""
 
 for animal in animals_data:
@@ -19,20 +19,33 @@ for animal in animals_data:
 
     # Name
     if "name" in animal:
-        animals_info += f"Name: {animal['name']}<br/>\n"
+        animals_info += f'<div class="card__title">{animal["name"]}</div>\n'
+
+    # Textbereich
+    animals_info += '<p class="card__text">\n'
 
     # Diet
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        animals_info += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+        animals_info += (
+            f"<strong>Diet:</strong> "
+            f"{animal['characteristics']['diet']}<br/>\n"
+        )
 
     # Location
     if "locations" in animal and animal["locations"]:
-        animals_info += f"Location: {animal['locations'][0]}<br/>\n"
+        animals_info += (
+            f"<strong>Location:</strong> "
+            f"{animal['locations'][0]}<br/>\n"
+        )
 
     # Type
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        animals_info += f"Type: {animal['characteristics']['type']}<br/>\n"
+        animals_info += (
+            f"<strong>Type:</strong> "
+            f"{animal['characteristics']['type']}<br/>\n"
+        )
 
+    animals_info += "</p>\n"
     animals_info += "</li>\n"
 
 
@@ -48,7 +61,7 @@ html_content = html_content.replace(
 )
 
 
-# Bestehende HTML-Datei aktualisieren
+# Vorhandene HTML-Datei aktualisieren
 with open("animals_template.html", "w") as file:
     file.write(html_content)
 
